d63202c942026cc1d264b24e987283d46b2b5998
Elle Li <yiran.li@hotmail.com>
2020-09-09 15:56:36 -0600

    Change the initial function for the feature use background img instead

--- main.js
+++ main.js
@@ -4,14 +4,28 @@
 
 // queryslector for each category button, and event listener
 var studyButton = document.querySelector('.study');
-var studyImg = document.querySelector('.study-img');
-var studyActiveImg = document.querySelector('.study-active-img')
+// var studyImg = document.querySelector('.study-img');
+// var studyActiveImg = document.querySelector('.study-active-img');
+// var meditateButton = document.querySelector('.meditate');
+// var meditateImg = document.querySelector('.meditate-img');
+// var meditateActiveImg = document.querySelector('.meditate-active-img')
 
-studyButton.addEventListener('click', changeColor)
+studyButton.addEventListener('click', changeStudyColor);
+// meditateButton.addEventListener('click', changeMeditateColor);
 // when button is clicked, calls function 
 // function: access the HTML elements(class) to change the color of the border, color of the title, image/color of image(different img in Assets)
-function changeColor() {
-    studyButton.classList.toggle('study-active')
-    studyImg.classList.toggle('hidden')
-    studyActiveImg.classList.toggle('hidden')
+
+ function changeColor(button,category) {
+     button.classList.toggle(category);
+//     img.classList.toggle('hidden');
+//     activeImg.classList.toggle('hidden');
 }
+
+ function changeStudyColor() {
+     changeColor(studyButton, 'study-active');
+}
+
+// function changeMeditateColor() {
+//     changeStudyColor()
+//     changeColor(meditateButton, meditateImg, meditateActiveImg, 'meditate-active');
+// }
