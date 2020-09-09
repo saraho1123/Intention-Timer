f7509610694e2f9133fc26d399f57cdeaa1c86d6
Elle Li <yiran.li@hotmail.com>
2020-09-09 14:42:51 -0600

    Pseudocode the feature

---
+++ main.js
@@ -0,0 +1,6 @@
+// When an activity category is clicked on (Exercise, Meditate, or Study), the associated border and icon should change colors to give a visual indication that it has been selected. Colors are provided in comp.
+
+// HTML+CSS for different category buttons for new colors
+// queryslector for each category button, and event listener
+// when button is clicked, calls function 
+// function: access the HTML elements(class) to change the color of the border, color of the title, image/color of image(different img in Assets)
