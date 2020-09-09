be9cf62c04ecda7f3cef7f33b67e430c0c6268ef
Elle Li <yiran.li@hotmail.com>
2020-09-09 16:56:43 -0600

    Complete change color feature

--- main.js
+++ main.js
@@ -4,28 +4,38 @@
 
 // queryslector for each category button, and event listener
 var studyButton = document.querySelector('.study');
-// var studyImg = document.querySelector('.study-img');
-// var studyActiveImg = document.querySelector('.study-active-img');
-// var meditateButton = document.querySelector('.meditate');
-// var meditateImg = document.querySelector('.meditate-img');
-// var meditateActiveImg = document.querySelector('.meditate-active-img')
+var meditateButton = document.querySelector('.meditate');
+var exerciseButton = document.querySelector('.exercise');
 
 studyButton.addEventListener('click', changeStudyColor);
+meditateButton.addEventListener('click', changeMeditateColor);
+exerciseButton.addEventListener('click', changeExerciseColor);
 // meditateButton.addEventListener('click', changeMeditateColor);
 // when button is clicked, calls function 
 // function: access the HTML elements(class) to change the color of the border, color of the title, image/color of image(different img in Assets)
 
- function changeColor(button,category) {
-     button.classList.toggle(category);
-//     img.classList.toggle('hidden');
-//     activeImg.classList.toggle('hidden');
+function changeColor(button, category) {
+    button.classList.add(category);
 }
 
- function changeStudyColor() {
-     changeColor(studyButton, 'study-active');
+function returnDefaultColor(button, category) {
+    button.classList.remove(category);
 }
 
-// function changeMeditateColor() {
-//     changeStudyColor()
-//     changeColor(meditateButton, meditateImg, meditateActiveImg, 'meditate-active');
-// }
+function changeStudyColor() {
+    changeColor(studyButton, 'study-active');
+    returnDefaultColor(meditateButton, 'meditate-active');
+    returnDefaultColor(exerciseButton, 'exercise-active');
+}
+
+function changeMeditateColor() {
+    changeColor(meditateButton, 'meditate-active');
+    returnDefaultColor(studyButton, 'study-active');
+    returnDefaultColor(exerciseButton, 'exercise-active');
+}
+
+function changeExerciseColor() {
+    changeColor(exerciseButton, 'exercise-active');
+    returnDefaultColor(studyButton, 'study-active');
+    returnDefaultColor(meditateButton, 'meditate-active');   
+}
