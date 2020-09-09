8a088a00c7f309a9a7b87986ad3f085cec3462e2
Aaron Townsend <atownse17@gmail.com>
2020-09-09 18:42:51 -0500

    Feature input fields limit range of minutes and seconds

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ var studyButton = document.querySelector('.study');
 var meditateButton = document.querySelector('.meditate');
 var exerciseButton = document.querySelector('.exercise');
 
+var accomplishInput = document.querySelector('.accomplish-input');
+var minutesInput = document.querySelector('.minutes-input');
+var secondsInput = document.querySelector('.seconds-input');
+
 studyButton.addEventListener('click', changeStudyColor);
 meditateButton.addEventListener('click', changeMeditateColor);
 exerciseButton.addEventListener('click', changeExerciseColor);
+
+minutesInput.addEventListener('keyup', limitChar)
+secondsInput.addEventListener('keyup', limitChar)
 
 function changeColor(button, category) {
     button.classList.add(category);
@@ -28,4 +35,20 @@ function changeMeditateColor() {
 function changeExerciseColor() {
     changeColor(exerciseButton, 'exercise-active');
     returnDefaultColor(studyButton, 'study-active', meditateButton, 'meditate-active');
+}
+
+// accessing accomplish, minutes, and seconds inputs
+// limiting the inputs of minutes and seconds to integers
+// limit minutes to two digits and seconds to under 60
+// prevent "e" from use in time inputs
+
+function limitChar() {
+    var minutes = parseInt(minutesInput.value);
+    var seconds = parseInt(secondsInput.value);
+    if (minutes > 90) {
+        minutesInput.value = '90';
+    }
+    if (seconds > 60) {
+        secondsInput.value = '60';
+    }
 }
